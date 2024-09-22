5c21bd8b9443e267f7d01ed40bd85ab5affebe66
Om-Prakash-13 <itsomprakashadhikari@gmail.com>
2024-09-22 18:25:18 +0530

    Optimized Algo., TC : O(log n)

--- Day-5/solution.java
+++ Day-5/solution.java
@@ -1,12 +1,21 @@
 public class solution {
     private static double myPow(double x, int n){
+        // Some Base case for bets time complexity.
+        if(x == 0) return 0;
+        if(x == 1 || n == 0 || (x == -1 && n % 2 == 0)) return 1;
+        if(n == 1) return x;
+        if(n == -1) return 1/x;
+        if(x == -1 && n % 2 != 0) return -1;
+
         double ans = 1.0;
         if(n < 0){
             x = 1/x;
             n *= -1;
         }
-        for(int i = 1; i <= n; i++){
-            ans *= x;
+        while (n != 0) {
+            if(n % 2 == 1) ans *= x;
+            x *= x;
+            n /= 2;
         }
         return ans;
     }
